ce8fe89de5e0664098e47595b2ce8cc648ba8030
HoseungJang <hsjang.dev@gmail.com>
2024-03-30 23:20:54 +0900

    fix title

--- src/__test__/array.test.ts
+++ src/__test__/array.test.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import { S } from "..";
 
-describe("Schema.Array", () => {
+describe("S.Array", () => {
   it("should return correct ArraySchema", () => {
     const numberSchema = S.Number();
     const arraySchema = S.Array(numberSchema);
